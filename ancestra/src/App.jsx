1edc176fd3bef3ea865a6775af90a6eaae254e81
Luis Moreno <chakiro527@gmail.com>
2021-11-11 20:12:31 -0500

    Agregado componente inicio y barra de navegacion

--- ancestra/src/App.jsx
+++ ancestra/src/App.jsx
@@ -1,12 +1,18 @@
-import './App.css';
-import Footer from './components/Footer';
-
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Inicio from "./pages/inicio/Inicio";
+import Productos from "./pages/productos/Productos";
 function App() {
   return (
-  <div className="App">
-    <Footer/>
-  </div>
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Inicio />} />
+          <Route path="/mostrar-productos" element={<Productos />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
--- ancestra/src/pages/Inicio/Inicio.jsx
+++ ancestra/src/pages/Inicio/Inicio.jsx
@@ -1,5 +1,13 @@
+import React from "react";
+import NavBar from "../../components/NavBar";
+import Home from "./components/Home";
 const Inicio = () => {
-    return (<div></div>  );
-}
- 
+  return (
+    <div className="p-3 pm-md-4 mx-auto text-center">
+      <NavBar />
+      <Home />
+    </div>
+  );
+};
+
 export default Inicio;
