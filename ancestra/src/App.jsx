c8cdf930fac809bcb2f5b342dab4682c1b146950
ngarcia1994 <77311387+ngarcia1994@users.noreply.github.com>
2021-12-02 18:58:42 -0500

    Update App.jsx

--- ancestra/src/App.jsx
+++ ancestra/src/App.jsx
@@ -7,6 +7,22 @@ function App() {
   return (
     <div className="App">
       <NavBar/>
+        function App() {
+          return (
+            <>
+            <Router>
+              <Navbar />
+              <Switch>
+                <Route path='/Home' component={Home} />
+                <Route path='/Productos' component={Productos} />
+                <Route path='/Accesorios&Hamacas' component={Accesorios&Hamacas} />
+                <Route path='/Login' component={Login} />
+                <Route path='/Carrito' component={Carrito} />
+              </Switch>
+            </Router>
+            </>
+           );
+        }
       <Inicio/>
       <Productos/>
       <Footer/>
